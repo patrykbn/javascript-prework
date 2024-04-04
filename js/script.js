4a137d1a004e25c745514e27c49d98f42e3cf17e
Patryk Bagan <patrykbn1997@gmail.com>
2024-04-04 22:42:19 +0200

    Wynik gry

--- js/script.js
+++ js/script.js
@@ -7,14 +7,13 @@ if(playerInput == '1'){
     playerMove = 'papier';
   } else if(playerInput == '3'){
     playerMove = 'nożyce';
+  } else {
+    playerMove = 'nieznany ruch';
   }
-  
   printMessage('Twój ruch to: ' + playerMove);
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-
 console.log('Wylosowana liczba to: ' + randomNumber);
-
 
 if(randomNumber == 1){
   computerMove = 'kamień';
@@ -23,5 +22,24 @@ if(randomNumber == 1){
 } else if(randomNumber == 3){
   computerMove = 'nożyce';
 }
-
 printMessage('Mój ruch to: ' + computerMove);
+
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+} else if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+} else if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+} else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Komputer wygrywa!');
+} else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Komputer wygrywa!');
+} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Komputer wygrywa!');
+}
