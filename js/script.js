3a2cee82a5c5f8e68144fd629d3a1e2b015d9260
Patryk Bagan <patrykbn1997@gmail.com>
2024-04-04 20:59:42 +0200

    pierwsze zadanie

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = 'kamień'
+let playerMove = 'papier'
+printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
