a556a0ad17ca6d1232853a93ea540a893fc57443
Patryk Bagan <patrykbn1997@gmail.com>
2024-04-04 22:02:52 +0200

    Odczytanie ruchów

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-let computerMove = 'kamień'
-let playerMove = 'papier'
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier , 3: nożyce')
+console.log('Gracz wpisał: ' + playerInput);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+if(playerInput == '1'){
+    playerMove = 'kamień';
+  } else if(playerInput == '2'){
+    playerMove = 'papier';
+  } else if(playerInput == '3'){
+    playerMove = 'nożyce';
+  }
+  
+  printMessage('Twój ruch to: ' + playerMove);
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+  computerMove = 'papier';
+} else if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
