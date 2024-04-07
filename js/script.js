f66eaf630b1dd7c1f5c00651eaab3d3f0fb63482
Patryk Bagan <patrykbn1997@gmail.com>
2024-04-07 22:02:03 +0200

    Update script.js otpimalisation #2

--- js/script.js
+++ js/script.js
@@ -6,34 +6,24 @@
     printMessage('Twój ruch to: ' + playerMove);
 
     const randomNumber = Math.floor(Math.random() * 3 + 1);
-    console.log('Wylosowana liczba to: ' + randomNumber);
 
     const computerMove = getMoveName(randomNumber);
     printMessage('Mój ruch to: ' + computerMove);
-    console.log('Ruch komputera to : ' + computerMove);
 
     const gameResult = displayResult(computerMove,playerMove);
     }
     
     function buttonRock(){
         playGame(1)
-        console.log('Player move- Rock');
     }
-
     function buttonPaper(){
         playGame(2)
-        console.log('Player move- Paper');
     }
-
     function buttonScissors(){
         playGame(3)
-        console.log('Player move- Scissors');
     }
 
     document.getElementById('play-rock').addEventListener('click', buttonRock);
-
     document.getElementById('play-paper').addEventListener('click', buttonPaper);
-
     document.getElementById('play-scissors').addEventListener('click', buttonScissors);
-
 }
